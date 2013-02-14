58bf29324f5a41c9d265fc2fe733f02920f294f6
esbaixauli <esbaixauli@d558d969-b249-81ea-e466-143c5232373f>
2013-02-14 13:16:29 +0000

    servidor

--- trunk/Implementación/CBRServer/src/servidorcbr/modelo/TipoCaso.java
+++ trunk/Implementación/CBRServer/src/servidorcbr/modelo/TipoCaso.java
@@ -1,16 +1,22 @@
 package servidorcbr.modelo;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 
-public class TipoCaso {
+public class TipoCaso implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
 	private String nombre;
 	private HashMap<String, Atributo> atbos;
-	private List<String> tecnicasRecuperacion;
-	private List<String> tecnicasReutilizacion;
-	private List<String> tecnicasRevision;
-	private List<String> tecnicasRetencion;
+	private List<Tecnica> tecnicasRecuperacion;
+	private List<Tecnica> tecnicasReutilizacion;
+	private List<Tecnica> tecnicasRevision;
+	private List<Tecnica> tecnicasRetencion;
+	private Tecnica defaultRec;
+	private Tecnica defaultReu;
+	private Tecnica defaultRev;
+	private Tecnica defaultRet;
 	
 	public String getNombre() {
 		return nombre;
@@ -28,36 +34,73 @@ public class TipoCaso {
 		this.atbos = atbos;
 	}
 	
-	public List<String> getTecnicasRecuperacion() {
+	public List<Tecnica> getTecnicasRecuperacion() {
 		return tecnicasRecuperacion;
 	}
 	
-	public void setTecnicasRecuperacion(List<String> tecnicasRecuperacion) {
+	public void setTecnicasRecuperacion(List<Tecnica> tecnicasRecuperacion) {
 		this.tecnicasRecuperacion = tecnicasRecuperacion;
 	}
 	
-	public List<String> getTecnicasReutilizacion() {
+	public List<Tecnica> getTecnicasReutilizacion() {
 		return tecnicasReutilizacion;
 	}
 	
-	public void setTecnicasReutilizacion(List<String> tecnicasReutilizacion) {
+	public void setTecnicasReutilizacion(List<Tecnica> tecnicasReutilizacion) {
 		this.tecnicasReutilizacion = tecnicasReutilizacion;
 	}
 	
-	public List<String> getTecnicasRevision() {
+	public List<Tecnica> getTecnicasRevision() {
 		return tecnicasRevision;
 	}
 	
-	public void setTecnicasRevision(List<String> tecnicasRevision) {
+	public void setTecnicasRevision(List<Tecnica> tecnicasRevision) {
 		this.tecnicasRevision = tecnicasRevision;
 	}
 	
-	public List<String> getTecnicasRetencion() {
+	public List<Tecnica> getTecnicasRetencion() {
 		return tecnicasRetencion;
 	}
 
-	public void setTecnicasRetencion(List<String> tecnicasRetencion) {
+	public void setTecnicasRetencion(List<Tecnica> tecnicasRetencion) {
 		this.tecnicasRetencion = tecnicasRetencion;
+	}
+	
+	public Tecnica getDefaultRec() {
+		return defaultRec;
+	}
+
+	public void setDefaultRec(Tecnica defaultRec) {
+		this.defaultRec = defaultRec;
+	}
+
+	public Tecnica getDefaultReu() {
+		return defaultReu;
+	}
+
+	public void setDefaultReu(Tecnica defaultReu) {
+		this.defaultReu = defaultReu;
+	}
+
+	public Tecnica getDefaultRev() {
+		return defaultRev;
+	}
+
+	public void setDefaultRev(Tecnica defaultRev) {
+		this.defaultRev = defaultRev;
+	}
+
+	public Tecnica getDefaultRet() {
+		return defaultRet;
+	}
+
+	public void setDefaultRet(Tecnica defaultRet) {
+		this.defaultRet = defaultRet;
+	}
+
+	@Override
+	public String toString() {
+		return nombre;
 	}
 	
 }
