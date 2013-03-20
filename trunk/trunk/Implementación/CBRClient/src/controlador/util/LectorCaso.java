f39851dbc62542796b2ec000940c54ff6731b268
rubenAnton90@gmail.com <rubenAnton90@gmail.com@d558d969-b249-81ea-e466-143c5232373f>
2013-03-20 15:10:14 +0000

    lector de casos

--- trunk/trunk/Implementación/CBRClient/src/controlador/util/LectorCaso.java
+++ trunk/trunk/Implementación/CBRClient/src/controlador/util/LectorCaso.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import servidorcbr.modelo.Atributo;
 import servidorcbr.modelo.TipoCaso;
 import weka.core.Attribute;
 import weka.core.Instance;
@@ -17,18 +16,23 @@ import weka.core.Instances;
 public class LectorCaso {
 	
 	private static List<HashMap<String,Object>> casos;
-
+	private static TipoCaso tic;
+	
 	public static List<HashMap<String,Object>> leerCaso(File f,TipoCaso tc) throws IOException {
 		BufferedReader reader = new BufferedReader(
                 new FileReader(f));
 		casos = new ArrayList<HashMap<String,Object>>();
 		Instances instancias = new Instances(reader);
+		tic=tc;
 		//por cada fila de las instancias (cada Instancia es un caso)
 		for(int i=0; i<instancias.numInstances();i++){
 			casos.add(new HashMap<String,Object>());
 			Instance instancia=instancias.instance(i);
 			for(int j = 0; j<tc.getAtbos().size();j++){
 				insertarAtributo(instancia,i,j);
+			}
+			if(casos.get(i).isEmpty()){
+				return null;
 			}
 		}
 		return casos;
@@ -39,10 +43,17 @@ public class LectorCaso {
 		String valor = instancia.toString(j);
 		String nombre = at.name();
 		Object o=valor;
-		if(at.isNumeric()){
-			o =Double.parseDouble(valor);
+		if(tic.getAtbos().containsKey(nombre)){
+			if(at.isNumeric()){
+				String tipoCaso = tic.getAtbos().get(nombre).getTipo();
+				if(tipoCaso.equals("I")){
+					o=Integer.parseInt(valor);
+				}else{
+					o =Double.parseDouble(valor);
+				}
+			}
+			casos.get(i).put(nombre,o);
 		}
-		casos.get(i).put(nombre,o);
 	} 
 	
 	
