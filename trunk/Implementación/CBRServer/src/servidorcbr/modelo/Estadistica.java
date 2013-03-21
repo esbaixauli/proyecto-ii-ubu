bd41c2880cce00bbe837b9fb1b7dacd599bcc472
rubenanton90 <rubenanton90@d558d969-b249-81ea-e466-143c5232373f>
2013-03-21 13:23:21 +0000

    servidorcbr/modelo/Estadistica.java

---
+++ trunk/Implementación/CBRServer/src/servidorcbr/modelo/Estadistica.java
@@ -0,0 +1,49 @@
+package servidorcbr.modelo;
+
+import java.util.Date;
+
+public class Estadistica {
+	private long ejecTotales;
+	private double mediaCalidad;
+	private long ejecSatisfactorias;
+	private long ejecInusables;
+	private Date fechaUltima;
+	
+	public long getEjecTotales() {
+		return ejecTotales;
+	}
+	public void setEjecTotales(long ejecTotales) {
+		this.ejecTotales = ejecTotales;
+	}
+	public double getMediaCalidad() {
+		return mediaCalidad;
+	}
+	public void setMediaCalidad(double mediaCalidad) {
+		this.mediaCalidad = mediaCalidad;
+	}
+	public long getEjecSatisfactorias() {
+		return ejecSatisfactorias;
+	}
+	public void setEjecSatisfactorias(long ejecSatisfactorias) {
+		this.ejecSatisfactorias = ejecSatisfactorias;
+	}
+	public long getEjecInusables() {
+		return ejecInusables;
+	}
+	public void setEjecInusables(long ejecInusables) {
+		this.ejecInusables = ejecInusables;
+	}
+	public Date getFechaUltima() {
+		return fechaUltima;
+	}
+	public void setFechaUltima(Date fechaUltima) {
+		this.fechaUltima = fechaUltima;
+	}
+	public long getCalidadUltima() {
+		return calidadUltima;
+	}
+	public void setCalidadUltima(long calidadUltima) {
+		this.calidadUltima = calidadUltima;
+	}
+	private long calidadUltima;
+}
