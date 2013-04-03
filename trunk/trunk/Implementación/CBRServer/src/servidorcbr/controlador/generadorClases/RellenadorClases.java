0acfd43c02a6a95b35f6dca19eeb15a51e1c937b
rubenAnton90@gmail.com <rubenAnton90@gmail.com@d558d969-b249-81ea-e466-143c5232373f>
2013-04-03 22:46:15 +0000

    servidor: rellenador de clases reestructurado y con nuevos metodos

--- trunk/trunk/Implementación/CBRServer/src/servidorcbr/controlador/generadorClases/RellenadorClases.java
+++ trunk/trunk/Implementación/CBRServer/src/servidorcbr/controlador/generadorClases/RellenadorClases.java
@@ -1,5 +1,6 @@
 package servidorcbr.controlador.generadorClases;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,11 +17,11 @@ import jcolibri.cbrcore.CaseComponent;
 public class RellenadorClases {
 
 
-	/** Adapta una lista de atributos con valores a una lista de CBRCases de jColibri.
+	/** Adapta varios conjuntos de atributos con valores a una lista de CBRCases de jColibri.
 	 * @param tc El tipo de caso al que pertenecen dichos atributos.
 	 * @param c los valores a adaptar.
 	 * @return una lista de CBRCases.
-	 * @throws ClassNotFoundException
+	 * @throws ClassNotFoundException Si la clase correspondiente a ese tipo de caso no se halla en el sistema de ficheros.
 	 * @see {@link CBRCase}
 	 */
 	public static List<CBRCase>	rellenarLista(TipoCaso tc,List<HashMap<String,Object>> c)
@@ -29,49 +30,10 @@ public class RellenadorClases {
 		//Cargo las clases del problema y la solucion
 		Class<?> desc=CargadorClases.cargarClaseProblema(tc.getNombre());
 		Class<?> sol=CargadorClases.cargarClaseProblema(tc.getNombre());
-		Class<?> tipo;
 		try{
 			//Por cada instancia de caso
 			for(HashMap<String, Object> caso:c){
-				//Creo una instancia del problema y de la solucion
-				CaseComponent instanciaDesc = (CaseComponent) desc.newInstance();
-				CaseComponent instanciaSol = (CaseComponent) sol.newInstance();
-				//Por cada par atributo-valor
-				for(Entry<String, Object> par  :caso.entrySet()){
-					//Si dicho atributo esta contenido en el tipo de caso
-					if(tc.getAtbos().containsKey(par.getKey())){
-						Atributo a = tc.getAtbos().get(par.getKey());
-						switch(a.getTipo()){
-							case "S":tipo=String.class;break;
-							case "I":tipo=Integer.class;break;
-							case "D":tipo=Double.class;break;
-							default:tipo=String.class;
-						}
-						//Si es un problema invoca al setter de dicho atributo en la clase del problema
-						if(a.getEsProblema()){
-							desc.getDeclaredMethod("set"+a.getNombre().substring(0,1).toUpperCase()+
-									a.getNombre().substring(1),tipo).invoke(instanciaDesc,par.getValue());
-							//Si además es el id del componente
-							if(a.getNombre().equals("id")){	
-								desc.getDeclaredMethod("setIdAttribute",Attribute.class)
-								.invoke(instanciaDesc,new Attribute("id", instanciaDesc.getClass()));
-							}
-							//Si no en la de la solución
-						}else{
-							desc.getDeclaredMethod("set"+a.getNombre().substring(0,1).toUpperCase()+
-									a.getNombre().substring(1),tipo).invoke(instanciaSol,par.getValue());
-							if(a.getNombre().equals("id")){
-								desc.getDeclaredMethod("setIdAttribute",Attribute.class).
-								invoke(instanciaSol,new Attribute("id", instanciaSol.getClass()));
-							}
-						}
-					
-					}
-				}
-				//Añado el caso a la lista
-				CBRCase cbrCase = new CBRCase();
-				cbrCase.setDescription(instanciaDesc);
-				cbrCase.setSolution(instanciaSol);
+				CBRCase cbrCase = obtenerCaso(tc, caso, desc, sol);
 				lista.add(cbrCase);
 			}
 		}catch(Exception ex){
@@ -79,6 +41,70 @@ public class RellenadorClases {
 		}
 		
 		return lista;
+	}
+	
+	/**Adapta un conjunto de atributos con valores a un CBRCase de jColibri.
+	 * @param tc  El tipo de caso al que pertenecen dichos atributos.
+	 * @param caso El valor a adaptar.
+	 * @return un CBRCase
+	 * @throws ClassNotFoundException Si la clase correspondiente a ese tipo de caso no se halla en el sistema de ficheros.
+	 * @see {@link CBRCase}
+	 */
+	public static CBRCase rellenarCaso(TipoCaso tc, HashMap<String,Object> caso) throws ClassNotFoundException{
+	
+		//Cargo las clases del problema y la solucion
+		Class<?> desc=CargadorClases.cargarClaseProblema(tc.getNombre());
+		Class<?> sol=CargadorClases.cargarClaseProblema(tc.getNombre());
+		try{
+			return obtenerCaso(tc, caso, desc, sol);
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+		return null;
+	}
+	
+	private static CBRCase obtenerCaso(TipoCaso tc, HashMap<String, Object> caso, Class<?>desc, Class<?>sol) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException{
+		//Creo una instancia del problema y de la solucion
+		CaseComponent instanciaDesc = (CaseComponent) desc.newInstance();
+		CaseComponent instanciaSol = (CaseComponent) sol.newInstance();
+		Class<?> tipo;
+		//Por cada par atributo-valor
+		for(Entry<String, Object> par  :caso.entrySet()){
+			//Si dicho atributo esta contenido en el tipo de caso
+			if(tc.getAtbos().containsKey(par.getKey())){
+				Atributo a = tc.getAtbos().get(par.getKey());
+				switch(a.getTipo()){
+					case "S":tipo=String.class;break;
+					case "I":tipo=Integer.class;break;
+					case "D":tipo=Double.class;break;
+					default:tipo=String.class;
+				}
+				//Si es un problema invoca al setter de dicho atributo en la clase del problema
+				if(a.getEsProblema()){
+					desc.getDeclaredMethod("set"+a.getNombre().substring(0,1).toUpperCase()+
+							a.getNombre().substring(1),tipo).invoke(instanciaDesc,par.getValue());
+					//Si además es el id del componente
+					if(a.getNombre().equals("id")){	
+						desc.getDeclaredMethod("setIdAttribute",Attribute.class)
+						.invoke(instanciaDesc,new Attribute("id", instanciaDesc.getClass()));
+					}
+					//Si no en la de la solución
+				}else{
+					desc.getDeclaredMethod("set"+a.getNombre().substring(0,1).toUpperCase()+
+							a.getNombre().substring(1),tipo).invoke(instanciaSol,par.getValue());
+					if(a.getNombre().equals("id")){
+						desc.getDeclaredMethod("setIdAttribute",Attribute.class).
+						invoke(instanciaSol,new Attribute("id", instanciaSol.getClass()));
+					}
+				}
+			
+			}
+		}
+		//Añado el caso a la lista
+		CBRCase cbrCase = new CBRCase();
+		cbrCase.setDescription(instanciaDesc);
+		cbrCase.setSolution(instanciaSol);
+		return cbrCase;
 	}
 	
 }
