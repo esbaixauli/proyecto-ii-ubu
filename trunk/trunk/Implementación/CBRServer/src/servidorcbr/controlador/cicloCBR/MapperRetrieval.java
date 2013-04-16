f435377733ad020e24942a78a625c22384ce6ecc
esbaixauli <esbaixauli@d558d969-b249-81ea-e466-143c5232373f>
2013-04-16 17:25:24 +0000

    servidor

--- trunk/trunk/Implementación/CBRServer/src/servidorcbr/controlador/cicloCBR/MapperRetrieval.java
+++ trunk/trunk/Implementación/CBRServer/src/servidorcbr/controlador/cicloCBR/MapperRetrieval.java
@@ -4,20 +4,34 @@ import java.io.IOException;
 
 import jcolibri.cbrcore.CBRCase;
 
+import org.apache.hadoop.hbase.HRegionLocation;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.IdentityTableMapper;
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
+import org.apache.hadoop.hbase.mapreduce.TableMapper;
 import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.mapreduce.Mapper;
 
-//public class MapperRetrieval extends Mapper<ImmutableBytesWritable, TableInputFormat, ImmutableBytesWritable, TableOutputFormat> {
-public class MapperRetrieval extends IdentityTableMapper {
+import servidorcbr.modelo.excepciones.PersistenciaException;
+import servidorcbr.persistencia.hbase.HbaseFacade;
+
+public class MapperRetrieval extends TableMapper<ImmutableBytesWritable, Result> {
 
 	@Override
-	public void map(ImmutableBytesWritable key, Result value, org.apache.hadoop.mapreduce.Mapper.Context context) throws IOException, InterruptedException {
-	//public void map(ImmutableBytesWritable key, TableInputFormat value, org.apache.hadoop.mapreduce.Mapper.Context context) throws IOException, InterruptedException {
-		super.map(key, value, context);
-		System.out.println("## Mapper ejecutandose");
+	public void map(ImmutableBytesWritable key, Result value, Mapper.Context context)
+					throws IOException, InterruptedException {
+		byte[] tableName = Bytes.toBytes(context.getConfiguration().get("tipocaso"));
+		byte[] row = key.copyBytes();
+		long regionId = 0;
+		try {
+			regionId = HbaseFacade.getInstance().getRegionId(tableName, row);
+		} catch (PersistenciaException e) {
+			e.printStackTrace();
+		}
+		System.out.println("## Mapper ejecutandose: region "+regionId);
+		byte[] region = Bytes.toBytes(regionId);
+		context.write(new ImmutableBytesWritable(region), value);
 	}
 }
