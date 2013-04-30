b7759710ec27dd7eecb9c8165799fd01bd20b0c3
rubenanton90 <rubenanton90@d558d969-b249-81ea-e466-143c5232373f>
2013-04-30 16:47:11 +0000

    cli ctrlCbr

--- trunk/trunk/Implementación/CBRClient/src/controlador/ControlCBR.java
+++ trunk/trunk/Implementación/CBRClient/src/controlador/ControlCBR.java
@@ -17,6 +17,13 @@ import servidorcbr.modelo.Usuario;
 
 public class ControlCBR {
 	
+	/**Envía la petición de recuperar. Usado en un ciclo paso a paso.
+	 * @param tc El tipo de caso al que pertenecen los casos.
+	 * @param query Consulta del usuario.
+	 * @return Casos recuperados.
+	 * @throws MalformedURLException Servlet incorrecto.
+	 * @throws IOException Error de conexión con el servlet.
+	 */
 	public static List<HashMap<String,Serializable>> retrieve(TipoCaso tc, HashMap<String,Serializable> query) throws MalformedURLException, IOException{
 		URLConnection con = ControlConexion.getServletCon("ServletCBR");
 		OutputStream outputStream = con.getOutputStream();
@@ -31,6 +38,45 @@ public class ControlCBR {
 			return (List<HashMap<String,Serializable>>)inputDelServlet.readObject();
 		} catch (ClassNotFoundException e) {}
 	    return null;
+	}
+	
+	/**Envía la petición de adaptación de los casos. Usado en un ciclo paso a paso.
+	 * @param tc El tipo de caso al que pertenecen los casos.
+	 * @param casos Lista de casos a adaptar.
+	 * @return Casos adaptados.
+	 * @throws IOException
+	 */
+	public static List<HashMap<String,Serializable>> reuse(TipoCaso tc,List<HashMap<String, Serializable>> casos) throws IOException{
+		URLConnection con = ControlConexion.getServletCon("ServletCBR");
+		OutputStream outputStream = con.getOutputStream();
+	    ObjectOutputStream oos = new ObjectOutputStream(outputStream);
+	    oos.writeObject("reuse");
+	    oos.writeObject(tc);
+	    oos.writeObject(casos);
+	    InputStream inputStream = con.getInputStream();
+	    ObjectInputStream inputDelServlet = new ObjectInputStream(inputStream);
+	    try {
+			return (List<HashMap<String,Serializable>>)inputDelServlet.readObject();
+		} catch (ClassNotFoundException e) {}
+	    return null;
+	}
+	
+	/**Envía la petición de revisión de un caso, para que el caso revisado quede en el servidor. 
+	 * @param tc El tipo de caso al que pertenece el caso.
+	 * @param caso El caso revisado.
+	 * @return Si la revisión tiene éxito y el nuevo caso se ha introducido en BD.
+	 * @throws IOException Si no se ha podido conectar.
+	 */
+	public static boolean revise(TipoCaso tc,HashMap<String, Serializable> caso) throws IOException{
+		URLConnection con = ControlConexion.getServletCon("ServletCBR");
+		OutputStream outputStream = con.getOutputStream();
+	    ObjectOutputStream oos = new ObjectOutputStream(outputStream);
+	    oos.writeObject("revise");
+	    oos.writeObject(tc);
+	    oos.writeObject(caso);
+	    InputStream inputStream = con.getInputStream();
+	    ObjectInputStream inputDelServlet = new ObjectInputStream(inputStream);
+		return inputDelServlet.readBoolean();   
 	}
 
 }
