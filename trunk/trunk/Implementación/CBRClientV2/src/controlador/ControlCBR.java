1d915a27baca8293bd8425465329ccc8442ebdf8
rubenanton90 <rubenanton90@d558d969-b249-81ea-e466-143c5232373f>
2013-05-24 09:00:22 +0000

    controlcbr

--- trunk/trunk/Implementación/CBRClientV2/src/controlador/ControlCBR.java
+++ trunk/trunk/Implementación/CBRClientV2/src/controlador/ControlCBR.java
@@ -25,19 +25,7 @@ public class ControlCBR {
 	 */
 	@SuppressWarnings("unchecked")
 	public static List<HashMap<String,Serializable>> retrieve(TipoCaso tc, HashMap<String,Serializable> query) throws MalformedURLException, IOException{
-		URLConnection con = ControlConexion.getServletCon("ServletCBR");
-		OutputStream outputStream = con.getOutputStream();
-	    ObjectOutputStream oos = new ObjectOutputStream(outputStream);
-	    oos.writeObject("retrieve");
-	    oos.writeObject(tc);
-	    oos.writeObject(query);
-	    InputStream inputStream = con.getInputStream();
-	    ObjectInputStream inputDelServlet = new ObjectInputStream(
-		          inputStream);
-	    try {
-			return (List<HashMap<String,Serializable>>)inputDelServlet.readObject();
-		} catch (ClassNotFoundException e) {}
-	    return null;
+		return enviarMensajeInicio(tc, query, "retrieve");
 	}
 	
 	/**Envía la petición de adaptación de los casos. Usado en un ciclo paso a paso.
@@ -81,6 +69,13 @@ public class ControlCBR {
 		return inputDelServlet.readBoolean();   
 	}
 
+	/**Envía el mensaje de Retención del ciclo cbr.
+	 * @param tc Tipo de Caso el caso
+	 * @param caso Instancia concreta del caso a retener.
+	 * @param u Usuario que realiza la retención.
+	 * @return true si éxito.
+	 * @throws IOException Si no se establece comunicación con el servlet.
+	 */
 	public static boolean retain(TipoCaso tc, HashMap<String, Serializable> caso, Usuario u) throws IOException {
 		URLConnection con = ControlConexion.getServletCon("ServletCBR");
 		OutputStream outputStream = con.getOutputStream();
@@ -92,6 +87,44 @@ public class ControlCBR {
 	    InputStream inputStream = con.getInputStream();
 	    ObjectInputStream inputDelServlet = new ObjectInputStream(inputStream);
 		return inputDelServlet.readBoolean();
+	}
+	
+	/**Ejecuta un ciclo completo hasta la revisión (que es manual), en una sola acción.
+	 * @param tc Tipo de caso del ciclo.
+	 * @param query Consulta del usuario.
+	 * @return Casos a revisar por el usuario procedentes del ciclo.
+	 * @throws MalformedURLException -
+	 * @throws IOException Error de conexión con el servlet.
+	 */
+	public static List<HashMap<String,Serializable>> cicloCompleto(TipoCaso tc, HashMap<String,Serializable> query) throws MalformedURLException, IOException{
+		return enviarMensajeInicio(tc, query, "completo");
+	}
+
+	/**Auxiliar. Envía un mensaje para iniciar el ciclo, ya sea un ciclo completo o paso a paso,
+	 * comenzando por el retrieve.
+	 * @param tc Tipo de caso del problema.
+	 * @param query Consulta del usuario.
+	 * @param mensaje "retrieve" para paso a paso, "completo" para ciclo completo.
+	 * @return Lista de casos recuperados (del retrieval o hasta la revisión, segun el mensaje).
+	 * @throws MalformedURLException -
+	 * @throws IOException Producida al conectar con el servlet.
+	 */
+	private static List<HashMap<String, Serializable>> enviarMensajeInicio(
+			TipoCaso tc, HashMap<String, Serializable> query, String mensaje)
+			throws MalformedURLException, IOException {
+		URLConnection con = ControlConexion.getServletCon("ServletCBR");
+		OutputStream outputStream = con.getOutputStream();
+	    ObjectOutputStream oos = new ObjectOutputStream(outputStream);
+	    oos.writeObject(mensaje);
+	    oos.writeObject(tc);
+	    oos.writeObject(query);
+	    InputStream inputStream = con.getInputStream();
+	    ObjectInputStream inputDelServlet = new ObjectInputStream(
+		          inputStream);
+	    try {
+			return (List<HashMap<String,Serializable>>)inputDelServlet.readObject();
+		} catch (ClassNotFoundException e) {}
+	    return null;
 	}
 
 }
