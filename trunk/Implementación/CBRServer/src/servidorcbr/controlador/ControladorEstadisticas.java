d7461f1c17e244ff5bbec63b486f0514ccc38847
esbaixauli <esbaixauli@d558d969-b249-81ea-e466-143c5232373f>
2013-05-31 17:56:30 +0000

    servidor::controlador: comentarios de clase y funcion

--- trunk/Implementación/CBRServer/src/servidorcbr/controlador/ControladorEstadisticas.java
+++ trunk/Implementación/CBRServer/src/servidorcbr/controlador/ControladorEstadisticas.java
@@ -6,17 +6,48 @@ import servidorcbr.modelo.Usuario;
 import servidorcbr.modelo.excepciones.PersistenciaException;
 import servidorcbr.persistencia.sql.SQLFacade;
 
+/**
+ * Clase estática de la capa de controlador para la gestión de estadísticas.
+ * @author Rubén Antón García, Enrique Sainz Baixauli
+ *
+ */
 public class ControladorEstadisticas {
 
 	
+	/**
+	 * Recupera de la base de datos el objeto estadística correspondiente a un usuario y un tipo
+	 * de caso. También se pueden obtener estadísticas de todos los usuarios o de todos los tipos
+	 * de caso cambiando el nombre del usuario o del tipo de caso por "ver todos".
+	 * @param u El usuario del que se quieren estadísticas, o "ver todos" para ver las globales.
+	 * @param tc El tipo de caso del que se quieren estadísticas, o "ver todos" para ver las globales.
+	 * @return El objeto Estadistica que contiene la información solicitada.
+	 * @throws PersistenciaException Si se produce algún error en la operación.
+	 */
 	public static Estadistica getEstadistica(Usuario u, TipoCaso tc) throws PersistenciaException{
 		return SQLFacade.getInstance().getEstadistica(u,tc);
 	}
 	
+	/**
+	 * Limpia las estadísticas correspondientes a un usuario y un tipo de caso. También puede
+	 * limpiar las estadísticas de todos los usuarios para un tipo de caso (con nombre de usuario
+	 * "ver todos"), las estadísticas de todos los tipos de caso para un mismo usuario (con nombre
+	 * de tipo de caso "ver todos") o todas las estadísticas almacenadas en el sistema (con ambos). 
+	 * @param u El usuario. Puede ser "ver todos" para eliminar las de todos los usuarios.
+	 * @param tc El tipo de caso. Puede ser "ver todos" para eliminar las de todos los tipos de caso.
+	 * @throws PersistenciaException Si se produce algún error en la operación.
+	 */
 	public static void limpiarEstadistica(Usuario u, TipoCaso tc) throws PersistenciaException{
 		SQLFacade.getInstance().limpiarEstadistica(u,tc);
 	}
 	
+	/**
+	 * Actualiza las estadísticas correspondientes a un usuario y un tipo de caso con una nueva
+	 * ejecución. Añade una ejecución de la calidad indicada a las estadísticas existentes.
+	 * @param u El usuario que ha ejecutado el ciclo del que proviene esta estadística.
+	 * @param tc El tipo de caso sobre el que se ha ejecutado el ciclo.
+	 * @param calidad La calidad que el usuario ha dado a esta ejecución.
+	 * @throws PersistenciaException Si se produce algún error en la operación.
+	 */
 	public static void updateEstadistica(Usuario u, TipoCaso tc, int calidad) throws PersistenciaException {
 		SQLFacade.getInstance().updateEstadistica(u, tc, calidad);
 	}
--- trunk/Implementación/CBRServer/src/servidorcbr/controlador/ControladorTipos.java
+++ trunk/Implementación/CBRServer/src/servidorcbr/controlador/ControladorTipos.java
@@ -12,17 +12,40 @@ import servidorcbr.modelo.excepciones.PersistenciaException;
 import servidorcbr.persistencia.hbase.HbaseFacade;
 import servidorcbr.persistencia.sql.SQLFacade;
 
+/**
+ * Clas estática de la capa de controlador para la gestión de tipos de caso.
+ * Permite añadir, borrar, recuperar o modificar tipos de caso.
+ * @author Rubén Antón García, Enrique Sainz Baixauli
+ *
+ */
 public class ControladorTipos {
 
+	/**
+	 * Recupera todos los tipos de caso almacenados en el sistema.
+	 * @return La lista de tipos de caso.
+	 * @throws PersistenciaException Si se produce algún error en la operación.
+	 */
 	public static List<TipoCaso> getTipos() throws PersistenciaException {
 		return SQLFacade.getInstance().getTipos();
 	}
 
+	/**
+	 * Recupera todos los tipos de caso a los que está asignado un determinado usuario.
+	 * @param u El usuario en cuestión.
+	 * @return La lista de tipos de caso.
+	 * @throws PersistenciaException Si se produce algún error en la operación.
+	 */
 	public static List<TipoCaso> getTipos(Usuario u)
 			throws PersistenciaException {
 		return SQLFacade.getInstance().getTipos(u.getNombre());
 	}
 
+	/**
+	 * Añade un nuevo tipo de caso al sistema.
+	 * @param tc El tipo de caso a añadir.
+	 * @return Si la operación tuvo éxito o no.
+	 * @throws PersistenciaException Si hubo algún error en la operación.
+	 */
 	public static boolean addTipo(TipoCaso tc) throws PersistenciaException {
 		boolean exito;
 		exito = SQLFacade.getInstance().addTipo(tc);
@@ -35,7 +58,12 @@ public class ControladorTipos {
 		return exito;
 	}
 
-	// Crea un mapa tipo de atributo:lista de atributos de ese tipo
+	/**
+	 * Crea un HashMap "tipo de atributo":"lista de atributos de ese tipo".
+	 * @param atbos El HashMap que contiene los atributos de un tipo de caso.
+	 * @param problema Si se quiere un mapa con los atributos son del problema (true) o de la solución (false).
+	 * @return El HashMap creado.
+	 */
 	private static HashMap<String, ArrayList<String>> creaMapa(
 			HashMap<String, Atributo> atbos, boolean problema) {
 		HashMap<String, ArrayList<String>> mapa = new HashMap<String, ArrayList<String>>();
@@ -51,11 +79,23 @@ public class ControladorTipos {
 		return mapa;
 	}
 
+	/**
+	 * Elimina un tipo de caso del sistema.
+	 * @param tc El tipo de caso a eliminar.
+	 * @return Si la operación tuvo éxito o no.
+	 * @throws PersistenciaException Si hubo algún error en la operación.
+	 */
 	public static boolean removeTipo(TipoCaso tc) throws PersistenciaException {
 		return SQLFacade.getInstance().removeTipo(tc.getNombre())
 				&& HbaseFacade.getInstance().dropTable(tc);
 	}
 
+	/**
+	 * Modifica un tipo de caso del sistema.
+	 * @param tc El tipo de caso modificado.
+	 * @return Si la operación tuvo éxito o no.
+	 * @throws PersistenciaException Si hubo algún error en la operación.
+	 */
 	public static boolean modifyTipo(TipoCaso tc) throws PersistenciaException {
 		return SQLFacade.getInstance().modifyTipo(tc);
 	}
