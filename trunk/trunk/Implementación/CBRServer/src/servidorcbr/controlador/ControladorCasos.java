408083cf944cb8519c39c4744be5bfb1e2754cd0
esbaixauli <esbaixauli@d558d969-b249-81ea-e466-143c5232373f>
2013-05-31 17:56:30 +0000

    servidor::controlador: comentarios de clase y funcion

--- trunk/trunk/Implementación/CBRServer/src/servidorcbr/controlador/ControladorCasos.java
+++ trunk/trunk/Implementación/CBRServer/src/servidorcbr/controlador/ControladorCasos.java
@@ -9,12 +9,31 @@ import servidorcbr.modelo.TipoCaso;
 import servidorcbr.modelo.excepciones.PersistenciaException;
 import servidorcbr.persistencia.hbase.HbaseFacade;
 
+/**
+ * Clase estática de controlador para gestión de casos: insertar casos y etapa de retención. 
+ * @author Rubén Antón García, Enrique Sainz Baixauli
+ *
+ */
 public class ControladorCasos {
 
+	/**
+	 * Inserta una lista de casos en Hbase.
+	 * @param tc El tipo de caso del que son instancia los casos.
+	 * @param casos Lista de casos a insertar.
+	 * @return Si hay éxito o no.
+	 * @throws PersistenciaException Si se produce un error en la operación.
+	 */
 	public static boolean putCasos (TipoCaso tc, List<HashMap<String,Serializable>> casos) throws PersistenciaException {
 		return HbaseFacade.getInstance().putCasos(tc, casos);
 	}
 
+	/**
+	 * Etapa de retención del Ciclo CBR. Simplemente, inserta el caso nuevo en la Base de Casos.
+	 * @param tc El tipo de caso del que es instancia el caso a retener.
+	 * @param caso El caso a insertar en la Base de Casos.
+	 * @return Si hay éxito o no.
+	 * @throws PersistenciaException Si se produce un error en la operación.
+	 */
 	public static boolean retain(TipoCaso tc, HashMap<String, Serializable> caso) throws PersistenciaException {
 		List<HashMap<String, Serializable>> casos = new ArrayList<HashMap<String, Serializable>>(1);
 		casos.add(caso);
--- trunk/trunk/Implementación/CBRServer/src/servidorcbr/controlador/ControladorUsuarios.java
+++ trunk/trunk/Implementación/CBRServer/src/servidorcbr/controlador/ControladorUsuarios.java
@@ -8,21 +8,38 @@ import servidorcbr.modelo.Usuario;
 import servidorcbr.modelo.excepciones.PersistenciaException;
 import servidorcbr.persistencia.sql.SQLFacade;
 
+/**
+ * Clase estática de la capa de controlador encargada de gestionar los usuarios del sistema.
+ * Permite añadir, eliminar, recuperar o modificar usuarios.
+ * @author Rubén Antón García, Enrique Sainz Baixauli
+ *
+ */
 public class ControladorUsuarios {
 
-	//Devuelve una tabla hash con los usuarios del sistema.
+	/**
+	 * Devuelve una tabla hash con los usuarios del sistema.
+	 * @return El HashMap con los usuarios.
+	 * @throws PersistenciaException Si se produjo algún error en la operación.
+	 */
 	public static HashMap<String,Usuario> getUsuarios() throws PersistenciaException{
 		return SQLFacade.getInstance().getUsuarios();
 	}
 	
+	/* Método no utilizado, está en ControladorTipos
 	//Devuelve los tipos de caso asociados a un usuario.
 	//(Un administrador tiene todos los tipos)
 	public static List<TipoCaso> getTipos(String usuario) throws PersistenciaException{
 		return SQLFacade.getInstance().getTipos(usuario);
-	}
+	}*/
 	
-	//Devuelve el usuario si se encontró y la contraseña es correcta,
-	//o null en caso contrario.
+	/**
+	 * Comprueba las credenciales de un usuario. Devuelve el objeto de tipo Usuario si encontró
+	 * el nombre y la contraseña o null en caso contrario.
+	 * @param nombre Nombre de usuario.
+	 * @param password Contraseña.
+	 * @return El usuario (si coinciden nombre y contraseña) o null (si no).
+	 * @throws PersistenciaException Si se produjo algún error en la operación.
+	 */
 	public static Usuario login(String nombre, String password) throws PersistenciaException{
 		Usuario resultado = null;
 		SQLFacade fachada = SQLFacade.getInstance();
@@ -35,15 +52,34 @@ public class ControladorUsuarios {
 		return resultado;
 	}
 	
-	// Devuelve cierto si el usuario se eliminó, o falso si no.
+	/**
+	 * Elimina un usuario del sistema.
+	 * @param u El usuario a eliminar.
+	 * @return Si la operación tuvo éxito.
+	 * @throws PersistenciaException Si se produjo algún error en la operación.
+	 */
 	public static boolean removeUsuario (Usuario u) throws PersistenciaException {
 		return SQLFacade.getInstance().removeUsuario(u);
 	}
 	
+	/**
+	 * Añade un usuario nuevo al sistema.
+	 * @param u El usuario a añadir.
+	 * @param casos Los casos que se le asignan inicialmente.
+	 * @return Si la operación tuvo éxito.
+	 * @throws PersistenciaException Si se produjo algún error en la operación.
+	 */
 	public static boolean addUsuario (Usuario u,List<String> casos) throws PersistenciaException {
 		return SQLFacade.getInstance().addUsuario(u,casos);
 	}
 	
+	/**
+	 * Modifica un usuario que existe en el sistema.
+	 * @param u El usuario a modificar.
+	 * @param casos Los casos que se le asignan.
+	 * @return Si la operación tuvo éxito.
+	 * @throws PersistenciaException Si se produjo algún error en la operación.
+	 */
 	public static boolean modUsuario (Usuario u,List<String> casos) throws PersistenciaException {
 		return SQLFacade.getInstance().modUsuario(u,casos);
 	}
